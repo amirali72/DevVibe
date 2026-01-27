3bd7a97ac56070ed768f7dc957da687fce2d636b
amirali72 <amirali531361@gmail.com>
2026-01-28 00:27:51 +0530

    Exploring GET POST DELETE HTTP call

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello")
+const user = ['amir']
+
+app.get("/user",(req,res)=>{
+    res.send(user)
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello from hello url")
+app.post("/user",(req,res)=>{
+    user.push("ali")
+    console.log(user);
+    
+    res.send(user)
+})
+
+app.use("/hellow",(req,res)=>{
+    res.send("Hello from hellow url")
 })
 
 app.listen(7777, ()=>{
