b87e2e3e67098d5d08e45775684d94c54cd88bde
amirali72 <amirali531361@gmail.com>
2026-01-28 00:50:47 +0530

    Playing with dynamic routes

--- app.js
+++ app.js
@@ -5,18 +5,31 @@ const app = express();
 const user = ['amir']
 
 app.get("/user",(req,res)=>{
-    res.send(user)
+    res.send({firstName:"Amir", lastName:"Ali"})
 })
 
 app.post("/user",(req,res)=>{
-    user.push("ali")
-    console.log(user);
-    
-    res.send(user)
+    console.log(req.body)
+    //saving to db
+    res.send("Data Saved successfully")
 })
 
-app.use("/hellow",(req,res)=>{
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully")
+})
+
+app.get(/.*fly$/,(req,res)=>{
     res.send("Hello from hellow url")
+})
+
+app.get("/test",(req,res)=>{
+    console.log(req.query);
+    res.send("Req Query Tested")
+})
+
+app.get("/paramstest/:name",(req,res)=>{
+    console.log(req.params);
+    res.send("Req Params Tested")
 })
 
 app.listen(7777, ()=>{
