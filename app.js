bb122dae71a0bdfaeb6019034d5f3b136bae77a1
amirali72 <amirali531361@gmail.com>
2026-01-29 16:28:22 +0530

    multiple-route-handlers

--- app.js
+++ app.js
@@ -4,34 +4,53 @@ const app = express();
 
 const user = ['amir']
 
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Amir", lastName:"Ali"})
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Amir", lastName: "Ali" })
 })
 
-app.post("/user",(req,res)=>{
+app.post("/user", (req, res) => {
     console.log(req.body)
     //saving to db
     res.send("Data Saved successfully")
 })
 
-app.delete("/user",(req,res)=>{
+app.delete("/user", (req, res) => {
     res.send("Deleted successfully")
 })
 
-app.get(/.*fly$/,(req,res)=>{
+app.get(/.*fly$/, (req, res) => {
     res.send("Hello from hellow url")
 })
 
-app.get("/test",(req,res)=>{
+app.get("/test", (req, res) => {
     console.log(req.query);
     res.send("Req Query Tested")
 })
 
-app.get("/paramstest/:name",(req,res)=>{
+app.get("/paramstest/:name", (req, res) => {
     console.log(req.params);
     res.send("Req Params Tested")
 })
 
-app.listen(7777, ()=>{
+app.use("/handlers", (req, res, next) => {
+    console.log("1st handler");
+    // res.send("1st handler")
+    next()
+}, (req, res, next) => {
+    console.log("2nd handler");
+    // res.send("2nd handler")
+    next()
+},[ (req, res, next) => {
+    console.log("3rd handler");
+    // res.send("3rd handler")
+    next()
+}, (req, res, next) => {
+    console.log("4th handler");
+    res.send("4th handler")
+    next()
+}],
+)
+
+app.listen(7777, () => {
     console.log("Server running");
 })
