e74a1cd8166eae259603b48a6b3f3069043e90a8
amirali72 <amirali531361@gmail.com>
2026-02-19 22:20:35 +0530

    created profile edit api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,14 +2,35 @@ const express = require('express');
 const profileRouter = express.Router();
 
 const { userAuth } = require('../middleware/auth');
+const { validateEditProfileData } = require('../utils/validation');
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
     try {
         const user = req.user;
         res.send(user);
     } catch (error) {
         res.status(400).send("Error:- " + error.message);
     }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+    try {
+        if (!validateEditProfileData(req)) {
+            throw new Error("Invalid Edit Request");
+        }
+
+        const loggedInUser = req.user;
+        console.log(loggedInUser);
+
+        Object.keys(req.body).forEach((key) => loggedInUser[key] = req.body[key]);
+
+        await loggedInUser.save();
+        res.json({ message: `${loggedInUser.firstName} - Updated Successfully`, data: loggedInUser });
+    } catch (error) {
+        res.status(400).send("Error:- " + error.message);
+    }
 })
+
+
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -15,4 +15,24 @@ const validateSignup = (req) => {
 
 }
 
-module.exports = {validateSignup}
+const validateEditProfileData = (req) => {
+    const allowedEditFields = ["firstName", "lastName", "emailId", "photoUrl", "gender", "age"];
+
+    const isEditAllowed = Object.keys(req.body).every((field) => allowedEditFields.includes(field));
+
+    if (req.body?.firstName && req.body.firstName.length < 3) {
+        throw new Error("Firstname must be at least 3 characters");
+    }
+    if (req.body?.lastName?.length < 3) {
+        throw new Error("Lastname must be at least 3 characters");
+    };
+
+    if (!validator.isEmail(req?.body?.emailId)) {
+        throw new Error("Invalid email address");
+    }
+
+
+    return isEditAllowed;
+}
+
+module.exports = { validateSignup, validateEditProfileData }
