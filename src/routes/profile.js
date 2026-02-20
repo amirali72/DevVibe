be58391f8045f51de22c32a372c620de85ba66d0
amirali72 <amirali531361@gmail.com>
2026-02-20 22:40:39 +0530

    added password edit api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const profileRouter = express.Router();
-
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 const { userAuth } = require('../middleware/auth');
 const { validateEditProfileData } = require('../utils/validation');
 
@@ -20,7 +21,6 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
         }
 
         const loggedInUser = req.user;
-        console.log(loggedInUser);
 
         Object.keys(req.body).forEach((key) => loggedInUser[key] = req.body[key]);
 
@@ -31,6 +31,26 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     }
 })
 
+profileRouter.patch("/profile/edit/password", userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        const { password } = req.body;
+        if (!validator.isStrongPassword(password)) {
+            throw new Error("Please add a strong password ");
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user.password = hashedPassword;
+        await user.save();
+        var token = await user.getJWT();
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + 7 * 24 * 3600000),
+            secure: true,
+        })
+        res.json({message:"Password Change Successfully"})
+    } catch (error) {
+        res.status(400).send("Error:- " + error.message);
+    }
 
+})
 
 module.exports = profileRouter;
